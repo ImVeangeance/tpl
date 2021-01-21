9fb505a87d4d4f9faf0dc57520e8abecaece5186
vengeance of the Dark Knight <vengeance@localhost.localdomain>
2021-01-21 14:42:29 +0700

    Clean some shit

--- main.py
+++ main.py
@@ -11,20 +11,6 @@ __theme__ = 'Написать программу, которая будет пр
             'несовпадения и в чём они состоят. Построить дерево вывода для любой выбранной ' \
             'цепочки из числа сгенерированных.'
 
-__help__ = ('Формат данных файла:\n'
-            'Программа принимает файлы в формате .json. Пример файла:\n'
-            '''{
-"VT": ["a", "b", "c"],
-"VN": ["A", "B", "C", "S"],
-"P": {
-    "S": ["AaB", "aB", "cC"],
-    "A": ["AB", "a", "b", "B"],
-    "B": ["Ba", ""],
-    "C": ["AB", "c"]
-},
-  "S": "S"
-}''')
-
 import json
 import sys
 
@@ -34,7 +20,7 @@ from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBo
 import utils
 from grammar import Grammar
 from ui import Ui_MainWindow
-from graphical_tree import GraphicalTree, Vertex
+#from graphical_tree import GraphicalTree, Vertex
 
 
 class MainWindow(QMainWindow):
@@ -50,7 +36,7 @@ def fill_form_with_grammar(grammar: Grammar):
     ui = window.ui
     ui.lineEdit.setText(', '.join(grammar.VT))
     ui.lineEdit_2.setText(', '.join(grammar.VN))
-    update_start_sym_combobox(grammar.S)
+    update_start_s(grammar.S)
     ui.plainTextEdit.clear()
     for sym, rules in grammar.P.items():
         rules = [rule if rule else lam for rule in rules]
@@ -63,7 +49,7 @@ def fill_form_with_grammar_h(grammar: Grammar):
     ui = window.ui
     ui.lineEdit.setText(', '.join(grammar.VT))
     #ui.lineEdit_2.setText(', '.join(grammar.VN))
-    update_start_sym_combobox(grammar.S)
+    update_start_s(grammar.S)
     ui.plainTextEdit_6.clear()
     ui.plainTextEdit_6.appendPlainText(grammar_to_str(grammar))
 
@@ -74,7 +60,7 @@ def open_grammar_file():
         try:
             grammar = Grammar(**json.load(open(filename)))
         except (json.JSONDecodeError, TypeError):
-            print_to_actions('Ошибка. Файл грамматики в неверном формате. '
+            print_to_pte5('Ошибка. Файл грамматики в неверном формате. '
                              'Откройте справку для получения информации о формате файла')
             return
         if not check_grammar(grammar):
@@ -82,60 +68,56 @@ def open_grammar_file():
         fill_form_with_grammar(grammar)
 
 
-def print_to_actions(string: str):
+def print_to_pte5(string: str):
     window.ui.plainTextEdit_5.appendPlainText(string)
 
 
 def check_grammar(grammar: Grammar) -> bool:
     if not grammar.VN:
-        print_to_actions("В грамматике отсутствует список нетерминальных символов!")
+        print_to_pte5("В грамматике отсутствует список нетерминальных символов!")
         return False
     if not grammar.VT:
-        print_to_actions("В грамматике отсутствует список терминальных символов!")
+        print_to_pte5("В грамматике отсутствует список терминальных символов!")
         return False
     if not grammar.S:
-        print_to_actions("Стартовый символ не задан!")
+        print_to_pte5("Стартовый символ не задан!")
         return False
     if intersection := set(grammar.VT) & set(grammar.VN):
-        print_to_actions(f'Символы {", ".join(intersection)} находятся как во множестве терминальных, так и во '
+        print_to_pte5(f'Символы {", ".join(intersection)} находятся как во множестве терминальных, так и во '
                          f'множестве нетерминальных символов. Грамматика задана неверно!')
         return False
     if grammar.S not in grammar.VN:
-        print_to_actions(f'Символ {grammar.S} не находится в списке терминальных символов. Грамматика задана неверно!')
+        print_to_pte5(f'Символ {grammar.S} не находится в списке терминальных символов. Грамматика задана неверно!')
         return False
     for sym, rules in grammar.P.items():
         if sym not in grammar.VN:
-            print_to_actions(f'Символ {sym} находится в списке правил, но его нет в списке нетерминальных символов. '
+            print_to_pte5(f'Символ {sym} находится в списке правил, но его нет в списке нетерминальных символов. '
                              f'Грамматика задана неверно!')
             return False
         if not rules:
-            print_to_actions(f'Для символа {sym} в грамматике отсутствуют правила!')
+            print_to_pte5(f'Для символа {sym} в грамматике отсутствуют правила!')
             return False
         if sym in grammar.VT:
-            print_to_actions(f'Символ {sym} находится в списке терминальных символов, но при этом используется '
+            print_to_pte5(f'Символ {sym} находится в списке терминальных символов, но при этом используется '
                              f'в левой части правил. Грамматика задана неверно!')
             return False
         for rule in rules:
             if rules.count(rule) != 1:
-                print_to_actions(f'Правило {rule} не может находиться в правой части правила более одного раза!')
+                print_to_pte5(f'Правило {rule} не может находиться в правой части правила более одного раза!')
                 return False
             for chain_sym in rule:
                 if chain_sym not in grammar.VN and chain_sym not in grammar.VT:
-                    print_to_actions(f'Символ {chain_sym} не находится ни во множестве терминальных, ни во множестве '
+                    print_to_pte5(f'Символ {chain_sym} не находится ни во множестве терминальных, ни во множестве '
                                      f'нетерминальных символов, но при этом есть в грамматике. '
                                      f'Грамматика задана неверно!')
                     return False
     return True
 
 
-def smth() -> None:
-    print(1111)
-
-
 def get_lambda() -> str:
     sym = window.ui.lineEdit_3.text()
     if not sym:
-        print_to_actions('Введите символ лямбды! Он не может быть пустым')
+        print_to_pte5('Введите символ лямбды! Он не может быть пустым')
     return sym
 
 
@@ -147,8 +129,8 @@ def read_grammar_from_form() -> Grammar:
         print(ui.plainTextEdit.toPlainText())
         p = utils.parse_rules(ui.plainTextEdit.toPlainText(), lam)
         print('Can gramm rules - ', p)
-    except utils.WrongRulesException as e:
-        print_to_actions(e.message)
+    except utils.WrongRule as e:
+        print_to_pte5(e.message)
         return False
     return Grammar(
         utils.split_by(ui.lineEdit.text(), ','),
@@ -166,8 +148,8 @@ def read_grammar_from_form_h(vn) -> Grammar:
         print('DEBUG - ', ui.plainTextEdit_6.toPlainText()[2])
         print('D - ', ui.plainTextEdit_6.toPlainText().split('P:')[1])
         p = utils.parse_rules(ui.plainTextEdit_6.toPlainText().split('P:')[1], lam)
-    except utils.WrongRulesException as e:
-        print_to_actions(e.message)
+    except utils.WrongRule as e:
+        print_to_pte5(e.message)
         return False
     return Grammar(
         utils.split_by(ui.lineEdit.text(), ','),
@@ -177,7 +159,7 @@ def read_grammar_from_form_h(vn) -> Grammar:
     )
 
 
-def update_start_sym_combobox(sym=None):
+def update_start_s(sym=None):
     cbox = window.ui.comboBox
     current = cbox.currentText()
     text = window.ui.lineEdit_2.text()
@@ -200,22 +182,22 @@ def grammar_to_str(grammar: Grammar) -> str:
 def remove_empty_and_chains_rules(grammar: Grammar) -> Grammar:
     child_free = grammar.find_child_free_non_terms()
     if child_free:
-        print_to_actions(f'Обнаружены бесплодные нетерминальные символы: {", ".join(child_free)}')
+        print_to_pte5(f'Обнаружены бесплодные нетерминальные символы: {", ".join(child_free)}')
         grammar = grammar.remove_rules(child_free)
-        print_to_actions('Грамматика после удаления бесплодных символов:\n' + grammar_to_str(grammar))
+        print_to_pte5('Грамматика после удаления бесплодных символов:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Бесплодных нетерминальных символов не обнаружено!')
+        print_to_pte5('Бесплодных нетерминальных символов не обнаружено!')
     unreacheble = grammar.find_unreachable_rules()
     if unreacheble:
-        print_to_actions(f'Обнаружены недостижимые нетерминальные символы: {", ".join(unreacheble)}')
+        print_to_pte5(f'Обнаружены недостижимые нетерминальные символы: {", ".join(unreacheble)}')
         grammar = grammar.remove_rules(unreacheble)
-        print_to_actions('Грамматика после удаления недостижимых символов:\n' + grammar_to_str(grammar))
+        print_to_pte5('Грамматика после удаления недостижимых символов:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Недостижимых нетерминальных символов не обнаружено!')
+        print_to_pte5('Недостижимых нетерминальных символов не обнаружено!')
     return grammar
 
 
-def save_result():
+def save_to_file():
     filename, _ = QFileDialog.getSaveFileName(filter='Text files (*.txt)')
     if not filename:
         return
@@ -235,37 +217,37 @@ def save_result():
 
 
 def build_canon_grammar(grammar: Grammar) -> Grammar:
-    print_to_actions('{: ^200}'.format('Начато приведение грамматики в каноничный вид'))
+    print_to_pte5('{: ^200}'.format('Начато приведение грамматики в каноничный вид'))
     grammar_before_delete_empty_rules_and_chains = grammar
     grammar = remove_empty_and_chains_rules(grammar)
     grammar = grammar.remove_empty_rules()
     if grammar != grammar_before_delete_empty_rules_and_chains:
-        print_to_actions('Удаление пустых правил...')
-        print_to_actions('Грамматика после удаления пустых правил:\n' + grammar_to_str(grammar))
+        print_to_pte5('Удаление пустых правил...')
+        print_to_pte5('Грамматика после удаления пустых правил:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Пустых правил не обнаружено!')
+        print_to_pte5('Пустых правил не обнаружено!')
     grammar_before_remove_chain_rulse = grammar
     grammar = grammar.remove_chain_rules()
     if grammar != grammar_before_remove_chain_rulse:
-        print_to_actions('Удаление цепных правил...')
-        print_to_actions('Грамматика после удаления цепных правил:\n' + grammar_to_str(grammar))
+        print_to_pte5('Удаление цепных правил...')
+        print_to_pte5('Грамматика после удаления цепных правил:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Цепных правил не обнаружено!')
+        print_to_pte5('Цепных правил не обнаружено!')
     grammar = remove_empty_and_chains_rules(grammar)
     if grammar_before_delete_empty_rules_and_chains == grammar:
-        print_to_actions('{: ^200}'.format('Грамматика поданная на вход изначально является канонической'))
+        print_to_pte5('{: ^200}'.format('Грамматика поданная на вход изначально является канонической'))
     else:
-        print_to_actions('{: ^200}'.format('Завершено приведение грамматики к каноничному виду\n'))
+        print_to_pte5('{: ^200}'.format('Завершено приведение грамматики к каноничному виду\n'))
     return grammar
 
 
-def calculate():
+def start():
     if not (grammar := read_grammar_from_form()):
         return
     if not check_grammar(grammar):
         return
     #
-    update_start_sym_combobox()
+    update_start_s()
     canon_grammar = build_canon_grammar(grammar)
     window.ui.plainTextEdit_2.setPlainText(grammar_to_str(canon_grammar))
     # print_to_actions()
@@ -285,53 +267,53 @@ def compare_chains():
 
 
 def holmsky(grammar: Grammar):
-    print_to_actions('{: ^200}'.format('Начато приведение канонической грамматики к бинарной грамматике Хомского'))
+    print_to_pte5('{: ^200}'.format('Начато приведение канонической грамматики к бинарной грамматике Хомского'))
     print(grammar)
     # step 1 - remove long rules (2 <)
     old_gr: Grammar.P = grammar.P
     grammar, rules, rules_count = grammar.remove_long_rules()
     if rules != old_gr:
-        print_to_actions(f'Грамматика после удаления длинных правил:\n{grammar_to_str(grammar)}')
+        print_to_pte5(f'Грамматика после удаления длинных правил:\n{grammar_to_str(grammar)}')
     else:
-        print_to_actions('Длинных правил не обнаружено!')
+        print_to_pte5('Длинных правил не обнаружено!')
     print(grammar)
     grammar_before_del_empty_rules = grammar
     grammar = grammar.remove_empty_rules()
     if grammar != grammar_before_del_empty_rules:
-        print_to_actions('Удаление пустых правил...')
-        print_to_actions('Грамматика после удаления пустых правил:\n' + grammar_to_str(grammar))
+        print_to_pte5('Удаление пустых правил...')
+        print_to_pte5('Грамматика после удаления пустых правил:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Пустых правил не обнаружено!')
+        print_to_pte5('Пустых правил не обнаружено!')
     grammar_before_remove_chain_rulse = grammar
     grammar = grammar.remove_chain_rules()
     if grammar != grammar_before_remove_chain_rulse:
-        print_to_actions('Удаление цепных правил...')
-        print_to_actions('Грамматика после удаления цепных правил:\n' + grammar_to_str(grammar))
+        print_to_pte5('Удаление цепных правил...')
+        print_to_pte5('Грамматика после удаления цепных правил:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Цепных правил не обнаружено!')
+        print_to_pte5('Цепных правил не обнаружено!')
     child_free = grammar.find_child_free_non_terms()
     if child_free:
-        print_to_actions(f'Обнаружены бесплодные нетерминальные символы: {", ".join(child_free)}')
+        print_to_pte5(f'Обнаружены бесплодные нетерминальные символы: {", ".join(child_free)}')
         grammar = grammar.remove_rules(child_free)
-        print_to_actions('Грамматика после удаления бесплодных символов:\n' + grammar_to_str(grammar))
+        print_to_pte5('Грамматика после удаления бесплодных символов:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Бесплодных нетерминальных символов не обнаружено!')
+        print_to_pte5('Бесплодных нетерминальных символов не обнаружено!')
     #
     old_gr = grammar
     grammar, rules = grammar.remove_trash()
     #
     fill_form_with_grammar_h(grammar)
     if grammar != old_gr:
-        print_to_actions(f'Уберём ситуации, когда в правиле встречаются несколько терминалов')
-        print_to_actions('Грамматика после изменения:\n' + grammar_to_str(grammar))
+        print_to_pte5(f'Уберём ситуации, когда в правиле встречаются несколько терминалов')
+        print_to_pte5('Грамматика после изменения:\n' + grammar_to_str(grammar))
     else:
-        print_to_actions('Повторений нескольких нетерминальных символов в правилах не обнаружено!')
+        print_to_pte5('Повторений нескольких нетерминальных символов в правилах не обнаружено!')
     h_sequences = grammar.make_chains(window.ui.spinBox.value(), window.ui.spinBox_2.value())
     h_sequences = [seq if seq else get_lambda() for seq in h_sequences]
     window.ui.plainTextEdit_4.setPlainText('\n'.join(sorted(h_sequences)))
-    print_to_actions('{: ^200}'.format('Окончено приведение канонической грамматики к бинарной грамматике Хомского'))
+    print_to_pte5('{: ^200}'.format('Окончено приведение канонической грамматики к бинарной грамматике Хомского'))
     print('grammar - ', grammar)
-    tree_create(grammar)
+    #tree_create(grammar)
     return grammar
 
 
@@ -354,7 +336,7 @@ def get_right_vertex(gr: Grammar, tree):
         if v:
             return gr, v
 
-
+'''
 def tree_create(grammar):
     QMessageBox.information(MainWindow(), "Статус", "Готовится простроение дерева, пожалуйста подождите...")
     choise = window.ui.lineEdit_4.text()
@@ -379,7 +361,7 @@ def tree_create(grammar):
     gt = GraphicalTree(Vertex(choise))
     gt.start()
     print('END')
-    ...
+    ...'''
 
 
 if __name__ == '__main__':
@@ -390,8 +372,8 @@ if __name__ == '__main__':
 
     window.show()
     grammar = False
-    update_start_sym_combobox()
-    window.ui.action.connect(SIGNAL('triggered()'), save_result)
+    update_start_s()
+    window.ui.action.connect(SIGNAL('triggered()'), save_to_file)
     window.ui.action_2.connect(SIGNAL('triggered()'), open_grammar_file)
     window.ui.action_3.connect(
         SIGNAL('triggered()'), lambda: QMessageBox.information(window, "Задание", __theme__)
@@ -399,7 +381,7 @@ if __name__ == '__main__':
     window.ui.action_4.connect(
         SIGNAL('triggered()'), lambda: QMessageBox.information(window, "Автор", 'ImVengeance\n16 вариант\nИП - 712')
     )
-    grammar = window.ui.pushButton.connect(SIGNAL('clicked()'), calculate)
+    grammar = window.ui.pushButton.connect(SIGNAL('clicked()'), start)
     #grammar = holmsky(grammar)
     print(grammar)
     window.ui.pushButton_4.connect(SIGNAL('clicked()'), window.ui.plainTextEdit_5.clear)
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def split_by(string: str, symbol: str) -> list[str]:
     return list(filter(None, map(str.strip, string.split(symbol))))
 
 
-class WrongRulesException(Exception):
+class WrongRule(Exception):
     def __init__(self, message):
         self.message = message
 
@@ -17,10 +17,10 @@ def parse_rules(rules_str: str, lambda_symbol: str) -> dict[str, list[str]]:
         left = left.strip()
         right = right.strip()
         if not sep:
-            raise WrongRulesException(f'В правиле {rule} отсутствует символ ->')
+            raise WrongRule(f'В правиле {rule} отсутствует символ ->')
         if not left:
-            raise WrongRulesException(f'В правиле {rule} отсутствует левая часть')
+            raise WrongRule(f'В правиле {rule} отсутствует левая часть')
         if not right:
-            raise WrongRulesException(f'В правиле {rule} отсутствует правая часть')
+            raise WrongRule(f'В правиле {rule} отсутствует правая часть')
         dictionary[left.strip()] = [seq if seq != lambda_symbol else '' for seq in split_by(right, '|')]
     return dictionary
